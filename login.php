a90e4ae270e030078fc325e6da04b53efa72b1f5
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2020-08-23 15:27:11 -0700

    bs: php: navbar added & included

--- login.php
+++ login.php
@@ -14,6 +14,7 @@
 </head>
 
 <body>
+  <?php include "parts/navbar.php"?>
   <h1>login Page</h1>
 
   <!-- Optional JavaScript -->
---
+++ parts/navbar.php
@@ -0,0 +1,12 @@
+<?php
+
+echo '<!-- Image and text -->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="index.html">
+    <img src="https://miro.medium.com/max/3902/0*_FE5Q4eSYpBO-aps" 
+    width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    Web Note
+  </a>
+</nav>';
+
+?>
