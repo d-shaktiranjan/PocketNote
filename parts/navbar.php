19d1d4d13fe9cf7882b82c95675cbcf0aa484c1d
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2020-08-25 05:41:52 -0700

    bs: use dark navabar

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,7 +1,7 @@
 <?php
 
 echo '<!-- Image and text -->
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-dark bg-dark">
   <a class="navbar-brand" href="index.php">
     <img src="https://miro.medium.com/max/3902/0*_FE5Q4eSYpBO-aps" 
     width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
