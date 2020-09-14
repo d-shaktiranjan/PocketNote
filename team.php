788b9064968b8a18c11b91f2ea594c1d13b92d87
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2020-09-14 15:06:40 -0400

    Thanks to @ResonantMatrix for this osm cards

---
+++ team.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team</title>
+    <link rel="stylesheet" href="teamStyle.css" />
+  </head>
+  <body>
+    <section class="card-list">
+      <article class="card">
+        <header class="card-header">
+          <p>Back-End Developer</p>
+          <h2>The Piro in ctrl+c & ctrl+v!!</h2>
+        </header>
+
+        <div class="card-author">
+          <a class="author-avatar" href="#">
+            <img src="./imgs/srd.jpg" />
+          </a>
+          <svg class="half-circle" viewBox="0 0 106 57">
+            <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+          </svg>
+
+          <div class="author-name">Shakti Ranjan Debata</div>
+        </div>
+        <div class="tags">
+          <a href="#">php</a>
+          <a href="#">Java</a>
+          <a href="#">Python</a>
+        </div>
+      </article>
+      <article class="card">
+        <header class="card-header">
+          <p>Front-End Developer</p>
+          <h2>The Pro Noob</h2>
+        </header>
+
+        <div class="card-author">
+          <a class="author-avatar" href="#">
+            <img src="./imgs/avatar.png" />
+          </a>
+          <svg class="half-circle" viewBox="0 0 106 57">
+            <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+          </svg>
+
+          <div class="author-name">Rahul Choudhury</div>
+        </div>
+        <div class="tags">
+          <a href="#">html</a>
+          <a href="#">css</a>
+          <a href="#">javascript</a>
+        </div>
+      </article>
+    </section>
+  </body>
+</html>
