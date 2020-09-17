c6dcef31edbae3f2d1339b4897936e59a31ab961
Rahul <rchoudhury63@gmail.com>
2020-09-17 23:09:16 +0530

    Changed theme to dark

--- login.php
+++ login.php
@@ -45,6 +45,8 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
     integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
+  <link rel="stylesheet" href="theme.css">
+
   <title>Login Page</title>
 </head>
 
--- signup.php
+++ signup.php
@@ -55,6 +55,8 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="theme.css">
+
     <title>Signup Page</title>
   </head>
   <body>
