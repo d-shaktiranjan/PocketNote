72dd46518a2336e1332608f66b5a32667895b42d
Rahul <rchoudhury63@gmail.com>
2020-09-17 23:07:41 +0530

    Replaced Icon

--- parts/navbar.php
+++ parts/navbar.php
@@ -20,7 +20,7 @@ if(($linkName==$lsHome) || ($linkName==$serverHomeName)){
 echo '<!-- Image and text -->
 <nav class="navbar navbar-dark bg-dark">
   <a class="navbar-brand" href="index.php">
-    <img src="https://miro.medium.com/max/3902/0*_FE5Q4eSYpBO-aps" 
+    <img src="./imgs/note.png" 
     width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
     Web Note
   </a>';
